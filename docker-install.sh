4424d25a203e940411d7932fd0aea6d7c72744f9
rahul213455 <96063693+rahul213455@users.noreply.github.com>
2024-03-14 13:15:23 +0530

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,25 @@
+# apt commands
+
+sudo apt update -y
+sudo apt install -y git wget
+curl https://get.docker.com/ | sh
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -a -G docker $USER
+id $USER
+newgrp docker
+sudo systemctl restart docker
+_______________________________
+_______________________________
+
+# yum commands
+
+sudo yum update -y
+sudo yum intsall -y git wget
+sudo yum install -y docker
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -a -G docker $USER
+id $USER
+newgrp docker
+sudo systemctl restart docker
