1832ee4e7ed94333a2f0bb316e21c336bc67a185
rahul213455 <96063693+rahul213455@users.noreply.github.com>
2024-06-15 15:39:03 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -22,3 +22,31 @@ sudo systemctl start docker
 sudo usermod -aG docker $USER && newgrp docker
 id $USER
 sudo systemctl restart docker
+
+# kubectl install
+
+sudo apt-get update
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg # allow unprivileged APT programs to read this keyring
+
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list   # helps tools such as command-not-found to work correctly
+
+sudo apt-get update
+
+sudo apt-get install -y kubectl
+
+kubectl cluster-info
+
+# install minikube
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
+minikube start
